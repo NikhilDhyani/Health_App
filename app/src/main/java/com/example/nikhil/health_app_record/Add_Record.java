ca4ebd92d90bbf25b490c0441bdb34bdad3dd673
nikhil dhyani <nikhildhyani365@gmail.com>
2018-06-06 19:05:00 +0530

    No null pointer exception now. SO link also provided. For better understanding.

--- app/src/main/java/com/example/nikhil/health_app_record/Add_Record.java
+++ app/src/main/java/com/example/nikhil/health_app_record/Add_Record.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Add_Record extends AppCompatActivity {
+public class Add_Record extends AppCompatActivity implements  View.OnClickListener{
 
     EditText name,bp_reading,sugar_reading;
     Button savenow;
@@ -16,25 +16,57 @@ public class Add_Record extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        /* PS : I have convert the following lines of code into
+                comments because I was getting null pointer exception.
+                Then after searching for it on SO & I got an answer why it happens.
+                 Link: https://stackoverflow.com/a/16838145/5003906
+
+       Explaination: We usually get nullpointer Exception with findViewById()
+                     when we initialise before setContentView().
+
+        Solution :  Always initialize after super.onCreate() and setContentView().
+
+       To, see please run it by uncommenting the code.
+
         name = findViewById(R.id.name);
         bp_reading = findViewById(R.id.BP);
         sugar_reading =  findViewById(R.id.sugar);
         savenow = (Button) findViewById(R.id.saveBtn);
 
+        */
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_add__record);
 
-        savenow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Whatsapp!",Toast.LENGTH_LONG).show();
+        name = findViewById(R.id.name);
+        bp_reading = findViewById(R.id.BP);
+        sugar_reading =  findViewById(R.id.sugar);
+        savenow = (Button) findViewById(R.id.saveBtn);
 
-            }
-        });
-      
+        savenow.setOnClickListener(this);
+
 
 
     }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+       switch (view.getId())
+       {
+           case R.id.saveBtn :  Toast.makeText(getApplicationContext(),"Whatsapp!",Toast.LENGTH_LONG).show();
+
+                                Intent intent = new Intent(Add_Record.this,MainActivity.class);
+                                startActivity(intent);
+
+       }
+
+
+    }
+
+
 
     /*public void clicknow(View view)
     {
